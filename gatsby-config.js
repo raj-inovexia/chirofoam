b3c78fca95b2784072c4e64bec99307480ff7582
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-23 20:04:00 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,30 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-shopify-admin`,
+      options: {
+        // The domain name of your Shopify shop. This is required.
+        // Example: 'gatsby-source-shopify-test-shop' if your Shopify address is
+        // 'gatsby-source-shopify-test-shop.myshopify.com'.
+        storeName:'chirofoam.myshopify.com',
+
+        // An API access token to your Shopify shop. This is required.
+        // You can generate an access token in the "Manage private apps" section
+        // of your shop's Apps settings. In the Storefront API section, be sure
+        // to select "Allow this app to access your storefront data using the
+        // Storefront API".
+        // See: https://help.shopify.com/api/custom-storefronts/storefront-api/getting-started#authentication
+        apiKey:'4d52a01cc9f9459adc48214472d0156a',
+        adminApiKey:'4d52a01cc9f9459adc48214472d0156a',
+
+        // Set verbose to true to display a verbose output on `npm run develop`
+        // or `npm run build`. This prints which nodes are being fetched and how
+        // much time was required to fetch and process the data.
+        // Defaults to true.
+        verbose: true,
+      },
+    },
+    {
       resolve: `gatsby-source-shopify`,
       options: {
         // The domain name of your Shopify shop. This is required.
