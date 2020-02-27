96c0f61a667d9ae58c9d0fd1bc50f0b6d9eb949b
Ankit Shukla <4nkit5hukla@gmail.com>
2020-02-27 08:37:56 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/pages/reviews.js
+++ src/pages/reviews.js
@@ -209,145 +209,146 @@ const Reviews = (props) => {
   </section>
 
   <section className="mb-0 py-5 position-relative">
-    <Container className="no-gutters">
-      <div className="col-md-12">
-        <Nav tabs={true} id="tabs" className="d-block">
-          <NavItem>
-            <NavLink className={activeTab === '1'
-                ? 'active'
-                : ''} onClick={() => setActiveTab('1')}>
-              Customer Reviews
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className={activeTab === '2'
-                ? 'active'
-                : ''} onClick={() => setActiveTab('2')}>
-              Leave A Review
-            </NavLink>
-          </NavItem>
-        </Nav>
-        <TabContent activeTab={activeTab}>
-          <TabPane tabId="1">
-            <div id="tabsContent" className="tab-content border border-top-0">
-              <div id="customer-revieew" className="tab-pane active show m-auto pb-5 position-relative" style={{
-                  width: '85%'
-                }}>
-                {
-                  (avgRating === 0) && <div className="h-100 w-100 bg-white d-flex justify-content-center align-items-center position-absolute" style={{
-                        zIndex: 1
-                      }}>
-                      <div className="spinner-border color-primary" role="status" style={{
-                          width: '5rem',
-                          height: '5rem',
-                          borderWidth: '.5rem'
+    <Container>
+      <Row className="mx-0">
+        <div className="col-md-12">
+          <Nav tabs={true} id="tabs" className="d-block">
+            <NavItem>
+              <NavLink className={activeTab === '1'
+                  ? 'active'
+                  : ''} onClick={() => setActiveTab('1')}>
+                Customer Reviews
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink className={activeTab === '2'
+                  ? 'active'
+                  : ''} onClick={() => setActiveTab('2')}>
+                Leave A Review
+              </NavLink>
+            </NavItem>
+          </Nav>
+          <TabContent activeTab={activeTab}>
+            <TabPane tabId="1">
+              <div id="tabsContent" className="tab-content border border-top-0">
+                <div id="customer-revieew" className="tab-pane active show m-auto pb-5 position-relative" style={{
+                    width: '85%'
+                  }}>
+                  {
+                    (avgRating === 0) && <div className="h-100 w-100 bg-white d-flex justify-content-center align-items-center position-absolute" style={{
+                          zIndex: 1
                         }}>
-                        <span className="sr-only">Loading...</span>
+                        <div className="spinner-border color-primary" role="status" style={{
+                            width: '5rem',
+                            height: '5rem',
+                            borderWidth: '.5rem'
+                          }}>
+                          <span className="sr-only">Loading...</span>
+                        </div>
+                      </div>
+                  }
+                  <Row className="mx-0">
+                    <Col sm="6" className="text-center py-0 py-sm-5 py-lg-5 py-xl-5">
+                      <p className="erbaum-bold color-secondary pt-5 mt-3">{avgRating}&nbsp; out of 5 stars</p>
+                      <p>
+                        <span>{totalRating}&nbsp;
+                        </span>
+                        reviews
+                      </p>
+                    </Col>
+                    <Col sm="6" className="py-5">
+                      <div className="p-0 list-unstyled review-details w-75 float-left float-sm-right float-lg-right float-xl-right">
+                        {
+                          Object.keys(overAllRating).reverse().map((index) => (<div key={index} className="w-100 d-flex color-primary mb-4">{index}<i className=" pl-1 pr-3 color-primary fa fa-star"></i>
+                            <div className="progress rounded-0 bg-transparent w-75 mt-1">
+                              <div className="progress-bar rounded-0 mr-4" style={{
+                                  width: ((overAllRating[index] / totalRating) * 100) + '%',
+                                  backgroundColor: 'rgb(186, 33, 84)'
+                                }}></div>
+                            </div>{overAllRating[index]}</div>))
+                        }
+                      </div>
+                    </Col>
+                  </Row>
+                  <Row className="mx-0">
+                    <div className="w-100 m-auto">
+                      <ul className="list-unstyled d-inline-block p-0 mb-0 ratings">
+                        {
+                          data.slice(0, showReviews).map((review, index) => (<li className="border mb-4" key={index}>
+                            <h4 className="color-primary erbaum-bold text-uppercase" style={{
+                                fontSize: '16px'
+                              }}>{review.product_title}</h4>
+                            <div className="d-inline-block br-widget br-readonly pt-2" title={"Rating: " + review.rating}>
+                              {
+                                [...Array(review.rating)].map((elem, i) => (<button data-rating-value={i} data-rating-text={i} className={(
+                                    (review.rating - 1) === i)
+                                    ? "p-0 border-0 bg-transparent p-0 border-0 bg-transparent"
+                                    : "br-selected p-0 border-0 bg-transparent p-0 border-0 bg-transparent"} key={i}>
+                                  <span className="color-primary fa fa-star"></span>
+                                </button>))
+                              }
+                              <div className="br-current-rating d-none">{review.rating}</div>
+                            </div>
+                            <p className="filson-pro-reg pt-2" style={{
+                                fontSize: '14px'
+                              }}>
+                              <b className="color-primary">{review.author}–</b>
+                              {getDate(review.created_at)}</p>
+                            <p className="filson-pro-reg text-1 color-secondary mb-0 pb-0">{review.body}</p>
+                          </li>))
+                        }
+                      </ul>
+                      <div className="position-relative">
+                        {
+                          (loadingReviews) && <div className="h-100 w-100 bg-white d-flex justify-content-center align-items-center position-absolute" style={{
+                                zIndex: 1
+                              }}>
+                              <div className="spinner-border color-primary" role="status" style={{
+                                  width: '3rem',
+                                  height: '3rem'
+                                }}>
+                                <span className="sr-only">Loading...</span>
+                              </div>
+                            </div>
+                        }
+                        {
+                          (data.length >= showReviews) && <p className="cta mt-0 mt-sm-3 pt-sm-4 pt-lg-4 pt-xl-4 mb-sm-2 pl-0 text-center">
+                              <button className="btn-cta color-primary erbaum-bold space-1 bg-transparent border-0 p-0" onClick={handleLoadMore} style={{
+                                  outline: 'none'
+                                }}>LOAD MORE</button>
+                            </p>
+                        }
                       </div>
                     </div>
-                }
-                <Row className="mx-0">
-                  <Col sm="6" className="text-center py-0 py-sm-5 py-lg-5 py-xl-5">
-                    <p className="erbaum-bold color-secondary pt-5 mt-3">{avgRating}&nbsp;
-                      out of 5 stars</p>
-                    <p>
-                      <span>{totalRating}&nbsp;
-                      </span>
-                      reviews
-                    </p>
-                  </Col>
-                  <Col sm="6" className="py-5">
-                    <div className="p-0 list-unstyled review-details w-75 float-left float-sm-right float-lg-right float-xl-right">
+                  </Row>
+                </div>
+              </div>
+            </TabPane>
+            <TabPane tabId="2">
+              <div id="tabsContent" className="tab-content border border-top-0">
+                <div id="leave-review" className="m-auto py-5 col-12 col-sm-10 col-lg-10 col-xl-10 p-0 px-sm-2">
+                  <Row className="mx-0">
+                    <div className="col-12 col-sm-12 col-lg-6 col-xl-6 m-auto d-flex no-gutters select-mattress">
                       {
-                        Object.keys(overAllRating).reverse().map((index) => (<div key={index} className="w-100 d-flex color-primary mb-4">{index}<i className=" pl-1 pr-3 color-primary fa fa-star"></i>
-                          <div className="progress rounded-0 bg-transparent w-75 mt-1">
-                            <div className="progress-bar rounded-0 mr-4" style={{
-                                width: ((overAllRating[index] / totalRating) * 100) + '%',
-                                backgroundColor: 'rgb(186, 33, 84)'
-                              }}></div>
-                          </div>{overAllRating[index]}</div>))
+                        allShopifyProduct.nodes.map((item, i) => (<Col key={i} className="col-6">
+                          <div className="card card-body text-center border-0 px-0 px-sm-2 px-lg-2 px-xl-2 mx-1">
+                            <button onClick={e => openModal(e, window.atob(item.shopifyId).split("/").pop(), item)} className="filson-pro-reg space-1 px-3 px-sm-4 px-lg-4 px-xl-4">{
+                                item.title.includes('XF')
+                                  ? 'Chirofoam X-Firm mattress'
+                                  : 'Chirofoam Premium Mattress'
+                              }</button>
+                            <p className="filson-pro-reg pt-4 color-secondary">Click here to add your reviews for our {item.title}</p>
+                          </div>
+                        </Col>))
                       }
                     </div>
-                  </Col>
-                </Row>
-                <Row className="mx-0">
-                  <div className="w-100 m-auto">
-                    <ul className="list-unstyled d-inline-block p-0 mb-0 ratings">
-                      {
-                        data.slice(0, showReviews).map((review, index) => (<li className="border mb-4" key={index}>
-                          <h4 className="color-primary erbaum-bold text-uppercase" style={{
-                              fontSize: '16px'
-                            }}>{review.product_title}</h4>
-                          <div className="d-inline-block br-widget br-readonly pt-2" title={"Rating: " + review.rating}>
-                            {
-                              [...Array(review.rating)].map((elem, i) => (<button data-rating-value={i} data-rating-text={i} className={(
-                                  (review.rating - 1) === i)
-                                  ? "p-0 border-0 bg-transparent p-0 border-0 bg-transparent"
-                                  : "br-selected p-0 border-0 bg-transparent p-0 border-0 bg-transparent"} key={i}>
-                                <span className="color-primary fa fa-star"></span>
-                              </button>))
-                            }
-                            <div className="br-current-rating d-none">{review.rating}</div>
-                          </div>
-                          <p className="filson-pro-reg pt-2" style={{
-                              fontSize: '14px'
-                            }}>
-                            <b className="color-primary">{review.author}–</b>
-                            {getDate(review.created_at)}</p>
-                          <p className="filson-pro-reg text-1 color-secondary mb-0 pb-0">{review.body}</p>
-                        </li>))
-                      }
-                    </ul>
-                    <div className="position-relative">
-                      {
-                        (loadingReviews) && <div className="h-100 w-100 bg-white d-flex justify-content-center align-items-center position-absolute" style={{
-                              zIndex: 1
-                            }}>
-                            <div className="spinner-border color-primary" role="status" style={{
-                                width: '3rem',
-                                height: '3rem'
-                              }}>
-                              <span className="sr-only">Loading...</span>
-                            </div>
-                          </div>
-                      }
-                      {
-                        (data.length >= showReviews) && <p className="cta mt-0 mt-sm-3 pt-sm-4 pt-lg-4 pt-xl-4 mb-sm-2 pl-0 text-center">
-                            <button className="btn-cta color-primary erbaum-bold space-1 bg-transparent border-0 p-0" onClick={handleLoadMore} style={{
-                                outline: 'none'
-                              }}>LOAD MORE</button>
-                          </p>
-                      }
-                    </div>
-                  </div>
-                </Row>
+                  </Row>
+                </div>
               </div>
-            </div>
-          </TabPane>
-          <TabPane tabId="2">
-            <div id="tabsContent" className="tab-content border border-top-0">
-              <div id="leave-review" className="m-auto py-5 col-12 col-sm-10 col-lg-10 col-xl-10 p-0 px-sm-2">
-                <Row className="mx-0">
-                  <div className="col-12 col-sm-12 col-lg-6 col-xl-6 m-auto d-flex no-gutters select-mattress">
-                    {
-                      allShopifyProduct.nodes.map((item, i) => (<Col key={i} className="col-6">
-                        <div className="card card-body text-center border-0 px-0 px-sm-2 px-lg-2 px-xl-2 mx-1">
-                          <button onClick={e => openModal(e, window.atob(item.shopifyId).split("/").pop(), item)} className="filson-pro-reg space-1 px-3 px-sm-4 px-lg-4 px-xl-4">{
-                              item.title.includes('XF')
-                                ? 'Chirofoam X-Firm mattress'
-                                : 'Chirofoam Premium Mattress'
-                            }</button>
-                          <p className="filson-pro-reg pt-4 color-secondary">Click here to add your reviews for our {item.title}</p>
-                        </div>
-                      </Col>))
-                    }
-                  </div>
-                </Row>
-              </div>
-            </div>
-          </TabPane>
-        </TabContent>
-      </div>
+            </TabPane>
+          </TabContent>
+        </div>
+      </Row>
     </Container>
   </section>
 
