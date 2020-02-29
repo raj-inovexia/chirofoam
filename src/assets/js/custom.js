fb23e0e2f55a0318cfb2a12d9264ee3b407f7171
RITU SHARMA <50765597+Sharma-Ritu@users.noreply.github.com>
2020-02-29 18:05:31 +0530

    Update custom.js

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -6,7 +6,7 @@
     script_tag.text += 'Shopify.shop = "chirofoam.myshopify.com";';
     var x = document.getElementsByTagName('script')[0];
     x.parentNode.insertBefore(script_tag, x);
-    var urls = ["https://www.google.com/recaptcha/api.js?render=explicit", "//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5e5a2594b9526dca"];
+    var urls = ["https://www.google.com/recaptcha/api.js?render=explicit", "//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5e5a59d03affc180"];
     for (var i = 0; i < urls.length; i++) {
       var s = document.createElement('script');
       s.type = 'text/javascript';
