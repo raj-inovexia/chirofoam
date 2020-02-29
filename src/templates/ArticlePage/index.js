e6850d53f67a4c622945d38d0c9a53dae2dce356
rajkumar-inovexia <rajkumar@innovagesoftwares.com>
2020-02-29 19:26:36 +0530

    Signed-off-by: rajkumar-inovexia <rajkumar@innovagesoftwares.com>

--- src/templates/ArticlePage/index.js
+++ src/templates/ArticlePage/index.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import Header from "~/components/header"
 import Footer from "~/components/footer"
-import {Row, Col} from 'reactstrap';
+import {Container, Row, Col, Form} from 'reactstrap';
 import SEO from '~/components/seo'
 import "~/assets/css/bootstrap.min.css"
 import RecentPosts from "~/components/Blogs/RecentPostsFooter"
 
 const ArticlePage = ({ data }) => {
   const article = data.shopifyArticle
-  console.log(article);
+  const totalcomments = article.comments.length
+  console.log(totalcomments);
   return (
     <>
     <SEO title={article.title} description={article.excerpt} />
     <Header />
     <section className="single-blog py-3 py-sm-3 py-lg-5 py-xl-5" style={{backgroundColor:'rgba(0,0,0,0.1)'}}>
-      <div className="container-large">
+      <div className="container">
         <h3 className="text-uppercase filson-pro-reg m-0" style={{fontSize:'22px'}}>{article.title}</h3>
       </div>
     </section>
     <section className="mb-0 py-5 position-relative">
-      <div className="container-large">
+      <div className="container">
         <Row>
           <Col sm="12" className="align-middle single-article">
 
@@ -50,22 +51,63 @@ const ArticlePage = ({ data }) => {
         </Row>
       </div>
     </section>
-    <section>
-      <div className="container">
-      
-             
-            {article.comments.map((comment,index) => (
-          <div className="">{comment.author.name}<br/>{comment.author.email}<br/>{comment.content}</div>
-         
-           ))
-         }
-      </div>
-    </section>
     <section className="rating-and-review py-3 py-sm-5 mb-4 mb-sm-0">
-      <div className="container-large pb-0 pb-sm-5">
+      <div className="container pb-0 pb-sm-5">
         <h3 className="text-center mb-4" style={{fontSize:'18px'}}>RECENT POSTS</h3>
         <RecentPosts />
       </div>
+    </section>
+    
+    <section id="comments" className="py-5 py-sm-3 py-md-3 py-lg-5 py-xl-5">
+      <Container>
+        <h3 className="text-center mb-4" style={{fontSize:'18px'}}>SHOWING {article.comments.length} COMMENTS</h3>
+             
+          {article.comments.map((comment,index) => (
+          <div className="mb-4">
+              <div className="profile-card pl-3">
+                <span><i class="fa fa-user-circle"></i></span><strong className="color-secondary filson-pro-reg pl-3 color-secondary" style={{fontSize:'12px'}}>{comment.author.name}</strong><p></p> <strong className="pl-3" style={{fontSize:'12px'}}><a href="#" className="color-secondary">Reply</a></strong>
+              </div>
+              <div className="comment-card p-3 position-relative mt-3">
+                  <p className="filson-pro-reg text-1 color-secondary">{comment.content}</p>
+              </div>
+            </div>
+           ))
+         }
+        
+      </Container>
+    </section>
+    <section className="py-5 py-sm-3 py-md-3 py-lg-5 py-xl-5">
+        <Container>
+            <h3 className="text-center mb-4" style={{fontSize:'18px'}}>LEAVE A COMMENT</h3>
+            <Row className="mx-0">
+                 <div className="comment-form w-100">
+                    <form method="post" action="">
+                        <Col className="col-12">
+                            <textarea placeholder="LEAVE YOUR COMMENT" className="w-100 text-1 color-secondary filson-pro-reg" rows="10" />
+                        </Col>
+                        <Row className="mx-0 input-data-field">
+                        <Col className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                            <input type="text" name="author" placeholder="Name (Required)" required="required" className="w-100 text-1 color-secondary filson-pro-reg" />
+                        </Col> 
+                        <Col className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                            <input type="email" name="email" placeholder="Email (Required)" required="required" className="w-100 text-1 color-secondary filson-pro-reg" />
+                        </Col>
+                        <Col className="col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4">
+                            <input type="text" name="website" placeholder="Website" className="w-100 text-1 color-secondary filson-pro-reg" />
+                        </Col>
+                        </Row>
+                        <Col sm="12">
+                          <input type="hidden" name="ip" />
+                          <input type="hidden" name="blog_id" />
+                          <input type="hidden" name="article_id" />
+                        </Col>
+                        <Col className="col-12">
+                            <button type="submit" className="comment-submit text-1 filson-pro-reg mt-3">POST COMMENT</button>
+                        </Col>
+                    </form>
+                 </div>
+            </Row>
+        </Container>
     </section>
     <Footer />
     </>
