38735ed3c113ec78ec88ded86e633bbe87c358fc
RITU SHARMA <50765597+Sharma-Ritu@users.noreply.github.com>
2020-03-02 17:08:48 +0530

    Update index.js

--- src/components/Blogs/index.js
+++ src/components/Blogs/index.js
@@ -88,12 +88,7 @@ const Blogs = ({ id }) => {
               
               <div className="blogs-content d-flex mt-4">
                 <Col className="blog-icon text-center pr-0 d-table col-1 border-right">
-                  <div className="addthis_inline_share_toolbox"></div>
-                  <FacebookShareButton url={fshareUrl}><FacebookIcon size={32} round={true} /></FacebookShareButton>
-                <TwitterShareButton url={tshareUrl}><TwitterIcon size={32} round={true} /></TwitterShareButton>
-                <PinterestShareButton url={pshareUrl}><PinterestIcon size={32} round={true} /></PinterestShareButton>
-                <LinkedinShareButton url={lshareUrl}><LinkedinIcon size={32} round={true} /></LinkedinShareButton>
-                  <p style={{color:'rgba(0,0,0,0.4)'}} className="pr-2 pr-sm-2 pr-lg-0 pr-xl-0"><i className="fa fa-share-alt"></i><span className="d-block">2</span></p>
+                 <p style={{color:'rgba(0,0,0,0.4)'}} className="pr-2 pr-sm-2 pr-lg-0 pr-xl-0"><i className="fa fa-share-alt"></i><span className="d-block">2</span></p>
                  <p className="border-top border-bottom py-2 pr-2 pr-sm-2 pr-lg-0 pr-xl-0" style={{color:'rgba(0,0,0,0.4)'}}><i className="fa fa-envelope"></i><span className="d-block">2</span></p>
                   <p className="mb-0 pr-2 pr-sm-2 pr-lg-0 pr-xl-0" style={{color:'rgba(0,0,0,0.4)'}}><i className="fa fa-heart"></i><span className="d-block">2</span></p>
                 </Col>
@@ -101,11 +96,10 @@ const Blogs = ({ id }) => {
                   <p style={{fontSize:'12px'}}>By <span>{author.name}</span> In <span>{blog.title}</span> Posted <span> {publishedAt}</span></p>
                   <h3 className="mb-3"><Link to={`/blogs/${blog.url.split("/").pop()}/${url.split("/").pop()}/`} state={{ fromFeed: true }} className="text-uppercase erbaum space-1 text-dark">{title}</Link></h3>
                   <p className="filson-pro-reg text-1">{excerpt}</p>
-                  <div className="addthis_inline_share_toolbox"></div>
-                  <FacebookShareButton url={fshareUrl}><FacebookIcon size={32} round={true} /></FacebookShareButton>
-                  <TwitterShareButton url={tshareUrl}><TwitterIcon size={32} round={true} /></TwitterShareButton>
-                  <PinterestShareButton url={pshareUrl}><PinterestIcon size={32} round={true} /></PinterestShareButton>
-                  <LinkedinShareButton url={lshareUrl}><LinkedinIcon size={32} round={true} /></LinkedinShareButton>
+                  <FacebookShareButton url={fshareUrl}><FacebookIcon size={16} round={true} /></FacebookShareButton>
+                  <TwitterShareButton url={tshareUrl}><TwitterIcon size={16} round={true} /></TwitterShareButton>
+                  <PinterestShareButton url={pshareUrl}><PinterestIcon size={16} round={true} /></PinterestShareButton>
+                  <LinkedinShareButton url={lshareUrl}><LinkedinIcon size={16} round={true} /></LinkedinShareButton>
                   <p className="cta mt-0 pt-sm-3 pt-lg-3 pt-xl-3 w-100">
                     <Link to={`/blogs/${blog.url.split("/").pop()}/${url.split("/").pop()}/`} state={{ fromFeed: true }} className="btn-cta color-primary erbaum-bold space-1">READ MORE</Link>
                   </p>
