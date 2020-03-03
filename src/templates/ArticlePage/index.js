bf5b33bc2776677755d7e7f1c0c56ccf5fc7eabf
Ankit Shukla <4nkit5hukla@gmail.com>
2020-03-03 12:26:26 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/templates/ArticlePage/index.js
+++ src/templates/ArticlePage/index.js
@@ -301,10 +301,10 @@ const ArticlePage = ({data}) => {
             </Row>
             <Row className="mx-0 input-data-field">
               <Col className="col-12">
-                  <button type="submit" className={"btn btn-custom-primary btn-lg text-1 filson-pro-reg mt-3 "+(
+                  <button type="submit" className={(
                       submitting)
-                      ? "color-primary position-relative"
-                      : "text-white"} style={{
+                      ? "btn btn-custom-primary btn-lg text-1 filson-pro-reg mt-3 color-primary position-relative"
+                      : "btn btn-custom-primary btn-lg text-1 filson-pro-reg mt-3 text-white"} style={{
                       opacity: 1
                     }} disabled={submitting}>
                     {
