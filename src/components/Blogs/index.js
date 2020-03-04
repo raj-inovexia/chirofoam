26ff3e02bd787946f168c0607819fac6df53ca66
Ankit Shukla <4nkit5hukla@gmail.com>
2020-03-04 15:48:46 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/components/Blogs/index.js
+++ src/components/Blogs/index.js
@@ -111,7 +111,7 @@ const Blogs = ({id}) => {
         //   console.error(error)
         // }).done()
       }
-      console.log(fetchLikeCount(`//icbtc.com/development/shopify-api/${reqData}`).resolve())
+      fetchLikeCount(`//icbtc.com/development/shopify-api/${reqData}`).then(res => console.log(res))
       return 0
     }else{
       return 0
