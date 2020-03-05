87baf400aee598fe0d6916cdf5e47e8132d04c1a
Ankit Shukla <4nkit5hukla@gmail.com>
2020-03-05 11:03:37 +0530

    Signed-off-by: Ankit Shukla <4nkit5hukla@gmail.com>

--- src/templates/ArticlePage/index.js
+++ src/templates/ArticlePage/index.js
@@ -224,14 +224,14 @@ const ArticlePage = ({data}) => {
                   color: 'rgba(0,0,0,0.4)'
                 }} className="ml-0 ml-sm-0 ml-lg-4 ml-xl-4">
                 <i className="fa fa-share-alt"></i>
-                <span className="pl-2">2</span>
+                <span className="pl-2">&nbsp;</span>
               </span>
 
-              <UncontrolledPopover trigger="legacy" placement="bottom" target="share">
+              <UncontrolledPopover placement="left" trigger="legacy" placement="bottom" target="share">
                 <PopoverBody>
                   <FacebookShareButton url={URL} className="p-1"><FacebookIcon size={25} round={true}/></FacebookShareButton>
                   <TwitterShareButton url={URL} className="p-1"><TwitterIcon size={25} round={true}/></TwitterShareButton>
-                  <PinterestShareButton media={article.image.src} url={URL} className="p-1"><PinterestIcon size={25} round={true}/></PinterestShareButton><br/>
+                  <PinterestShareButton media={article.image.src} url={URL} className="p-1"><PinterestIcon size={25} round={true}/></PinterestShareButton>
                   <LinkedinShareButton url={URL} className="p-1"><LinkedinIcon size={25} round={true}/></LinkedinShareButton>
                 </PopoverBody>
               </UncontrolledPopover>
