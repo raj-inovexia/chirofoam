b7073ccbd8a8bb235e40a07a99c61d3aa541c04f
RITU SHARMA <50765597+Sharma-Ritu@users.noreply.github.com>
2020-03-05 20:14:53 +0530

    Update our-story.js

--- src/pages/our-story.js
+++ src/pages/our-story.js
@@ -12,9 +12,7 @@ import support from "../assets/img/support.jpg"
 import us from "../assets/img/pur.png"
 import packing from "../assets/img/unpacking-chirofoam.jpg"
 import logo from "../assets/img/logo2.png"
-
-
-
+import ScrollAnimation from 'react-animate-on-scroll'
 
 export default (props) => {
   return (
@@ -22,16 +20,18 @@ export default (props) => {
 		<SEO title="INTRODUCING THE CHIROFOAM™ MATTRESS" />
 		<Header />
 		<section className="blue-bg">
+		<ScrollAnimation animateIn="fadeInUp">
 			<Jumbotron className="mb-0 text-center text-white bg-transparent space-1">
 				<h1 className="font-weight-bold display-5 erbaum-bold text-uppercase pt-5 space-2">Introducing The Chirofoam™ Mattress</h1>
 			</Jumbotron>
+		</ScrollAnimation>
 		</section>
 		<section className="mb-0 py-3 position-relative">
 			<Container className="our-focus pt-5">
 				<h4 className="text-center lead-text-font color-primary erbaum-bold text-uppercase w-100 pb-lg-3 pb-xl-3 m-0 pb-2 pb-sm-3">Our focus on comfort, support, and health</h4>
 				<Row className="mb-2 mb-sm-5 pb-0 pb-sm-5 pt-2 pt-sm-2 pt-lg-5 pt-xl-5 mx-0">
 					<div className="col-12 col-sm-8 col-lg-8 col-xl-8 left-card">
-						<h5 className="erbaum-bold color-primary pb-2 pb-lg-3 pb-xl-3">TODAY’S MATTRESS MARKET</h5>
+						<h5 className="erbaum-bold color-primary pb-2 pb-lg-3 pb-xl-3"><ScrollAnimation animateIn="fadeInUp">TODAY’S MATTRESS MARKET</ScrollAnimation></h5>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">With so many options on the market today, there are hundreds of various mattresses to choose from ranging in firmness, features, coils, foams, cooling gels, fabrics and everything in between.</p>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">But which mattress is best for you? Firm or soft? Coils or foam? With so much information on the internet today, the mattress buying experience can be more confusing than ever before.</p>
 						<p className="filson-pro-reg color-secondary text-1">At Chirofoam™, we decided to transform the mattress buying experience and take the guess work out of buying the perfect mattress.</p>
@@ -45,14 +45,14 @@ export default (props) => {
 						<img src={women} alt="women-sleeping"/>
 					</div>
 					<div className="col-12 col-sm-8 col-lg-8 col-xl-8 left-card">
-						<h5 className="erbaum-bold color-primary pb-2 pb-lg-3 pb-xl-3">A GREAT MATTRESS IS FOR YOUR BODY AND MIND</h5>
+						<h5 className="erbaum-bold color-primary pb-2 pb-lg-3 pb-xl-3"><ScrollAnimation animateIn="fadeInUp">A GREAT MATTRESS IS FOR YOUR BODY AND MIND</ScrollAnimation></h5>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">Whether you experience back pain or not, sleeping on the right mattress can have a positive effect on your health and overall well being.  A lot of back pain can be attributed to an outdated mattress, as well as improper posture, or strenuous physical activity throughout the day.  The right mattress will help your body recover at night to prevent as well as relieve common back pain.</p>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">The Chirofoam™ mattress was developed with a focus on muscle recovery, to help relieve back pain and tension in the body.  Restorative sleep is also very important to mental health, alertness, and energy throughout the day. If you let your mind rest easy at night, it will reward you by performing better during the day.</p>
 					</div>
 				</Row>
 				<Row className="mb-2 mb-sm-5 pb-0 pb-sm-5 mx-0">
 					<div className="col-12 col-sm-8 col-lg-8 col-xl-8 left-card">
-						<h5 className="erbaum-bold color-primary pb-2 pb-lg-3 pb-xl-3">DEVELOPING THE PERFECT MATTRESS</h5>
+						<h5 className="erbaum-bold color-primary pb-2 pb-lg-3 pb-xl-3"><ScrollAnimation animateIn="fadeInUp">DEVELOPING THE PERFECT MATTRESS</ScrollAnimation></h5>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">At Chirofoam™, we have worked together with chiropractors, mattress experts, as well as consumers like yourself to design and develop the perfect mattress.</p>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">Our Chiropractors insisted that that the perfect mattress needs to have factors of conformity, alignment, support, pressure relief, and comfort.  They focused on restorative sleep which targets muscle recovery and back pain relief.</p>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">Our mattress experts insisted that the perfect mattress needs to provide pressure relief, proper back support and alignment, provide a balanced sleep temperature, eliminate motion transfer, and resist sagging. They focused on optimum sleep comfort.</p>
@@ -69,7 +69,7 @@ export default (props) => {
 						<img src={us} alt="us"/>
 					</div>
 					<div className="col-12 col-sm-8 col-lg-8 col-xl-8 left-card">
-						<h5 className="erbaum-bold color-primary pb-2 pb-lg-3 pb-xl-3">QUALITY MATERIALS AND SKILLED CRAFTSMANSHIP</h5>
+						<h5 className="erbaum-bold color-primary pb-2 pb-lg-3 pb-xl-3"><ScrollAnimation animateIn="fadeInUp">QUALITY MATERIALS AND SKILLED CRAFTSMANSHIP</ScrollAnimation></h5>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">With hundreds of hours of design and development, we have found the perfect mix of firmness, support, and comfort to help you get that sleep you’ve always dreamed of.</p>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">We have sourced the best technologies and highest quality materials, which will not only stand up to the test of time, but are also environmentally responsible and healthy for you to sleep on night after night.</p>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">Our foams are Certipur certified with ultra-low VOC with the highest environmental production standards. Our foams are made without ozone depleters, do not contain any mercury, lead or any other heavy metals, and are made without formaldehyde or phthalates.  Each mattress is also wrapped in a flame retardant sock, free of any harmful chemicals.</p>
@@ -78,7 +78,7 @@ export default (props) => {
 				</Row>
 				<Row className="mb-2 mb-sm-5 pb-0 pb-sm-5 mx-0">
 					<div className="col-12 col-sm-8 col-lg-8 col-xl-8 left-card">
-						<h5 className="erbaum-bold color-primary pb-2 pb-lg-3 pb-xl-3">EASY DELIVERY AND SET UP IN YOUR HOME</h5>
+						<h5 className="erbaum-bold color-primary pb-2 pb-lg-3 pb-xl-3"><ScrollAnimation animateIn="fadeInUp">EASY DELIVERY AND SET UP IN YOUR HOME</ScrollAnimation></h5>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">Not only did we develop the perfect mattress, we have sourced a way to conveniently package it in an easy to deliver box, which can be shipped across Canada and the USA, and enjoyed by everyone quickly and easily.</p>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">Our foams are of such high quality that we are able to compress the mattress into a portable box and ship directly to your door for easy setup. The Chirofoam™ mattress is tough enough to come back to its original shape, and offer you many years of support and comfort while you sleep.</p>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">This method of shipping allows us to demonstrate the integrity of the Chirofoam™ mattress.   We are able to ship the mattress to your door at a fraction of the cost, saving you money and reducing your carbon footprint on our planet.</p>
@@ -92,7 +92,7 @@ export default (props) => {
 						<img src={logo} alt="chirofoam"/>
 					</div>
 					<div className="col-12 col-sm-8 col-lg-8 col-xl-8 left-card">
-						<h5 className="erbaum-bold color-primary pb-2 pb-lg-3 pb-xl-3">WE GUARANTEEE YOUR SATISFACTION</h5>
+						<h5 className="erbaum-bold color-primary pb-2 pb-lg-3 pb-xl-3"><ScrollAnimation animateIn="fadeInUp">WE GUARANTEEE YOUR SATISFACTION</ScrollAnimation></h5>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">We are so confident that the Chirofoam™ mattress is going to be the best mattress you have ever slept on that we are offering a 100 night in home trial to try it out for yourself. We love to hear our customer’s feedback, so please share and tell us how Chirofoam™ has transformed your life.</p>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">From our team at Chirofoam™, we wish you many years of comfortable, relaxing, and rejuvenating sleep on your new Chirofoam™ mattress.</p>
 					</div>
@@ -119,6 +119,7 @@ export default (props) => {
 				</Row>
 			</Container>
 		</section>
+		<ScrollAnimation animateIn="fadeInUp">
 		<section className="rating-and-review py-3 py-sm-5 mb-4 mb-sm-0">
 			<Container className="pb-0 pb-sm-5">
 				<Row className="mx-0">
@@ -137,7 +138,7 @@ export default (props) => {
 				</Row>
 			</Container>
 		</section>
-
+</ScrollAnimation>
 		<Footer />
 	</>
   );
