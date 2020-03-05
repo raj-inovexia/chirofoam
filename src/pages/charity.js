fe8fb22697a3d528ca8bd3c84f1124d68e5e1a23
RITU SHARMA <50765597+Sharma-Ritu@users.noreply.github.com>
2020-03-05 20:08:55 +0530

    Update charity.js

--- src/pages/charity.js
+++ src/pages/charity.js
@@ -10,6 +10,7 @@ import children from "../assets/img/children.jpg"
 import bedkit from "../assets/img/bedkit.jpg"
 import donate from "../assets/img/donate.jpg"
 import dream from "../assets/img/dream.jpg"
+import ScrollAnimation from 'react-animate-on-scroll'
 
 export default (props) => {
   return (
@@ -17,16 +18,18 @@ export default (props) => {
 		<SEO title="SLEEPING CHILDREN AROUND THE WORLD" description="Chirofoam™ will donate 1 Bedkit for every mattress sold." />
 		<Header />
 		<section className="blue-bg charity">
+		<ScrollAnimation animateIn="fadeInUp">
 			<Jumbotron className="mb-0 text-center text-white bg-transparent space-1 w-100 m-auto">
 				<h1 className="display-5 font-weight-bold erbaum-bold text-uppercase pt-md-5 space-2 pt-sm-1">Sleeping Children Around The World</h1>
 				<p className="space-4 proxima-r text-white m-auto text-center px-0 px-sm-5 px-lg-5 px-xl-5 pt-2 pt-sm-4" style={{width:'90%',fontSize:'26px'}}>Chirofoam™ will donate 1 Bedkit for every mattress sold.</p>
 			</Jumbotron>
+		</ScrollAnimation>
 		</section>
 		<section className="mb-0 py-3 position-relative">
 			<Container>
 				<Row className="charity-inner text-center mx-0">
 					<Col sm="6" className="mb-3 mb-sm-5 mt-3 mt-sm-5 px-0 px-sm-2 px-md-2 px-lg-4 px-xl-4">
-						<h5 className="erbaum-bold color-primary pb-2 pb-sm-5 text-uppercase">The Charity’s Purpose </h5>
+						<h5 className="erbaum-bold color-primary pb-2 pb-sm-5 text-uppercase"><ScrollAnimation animateIn="fadeInUp">The Charity’s Purpose </ScrollAnimation></h5>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">Since it’s founding in 1970, Sleeping Children Around The World (SCAW) has raised over $40 million to provide bedkits for children in 34 countries.  They have helped over one million children by 2009.</p>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">Their purpose is crystal clear: give every child the opportunity to have a good night’s sleep.  With sleep, the promise of a better tomorrow is realized.</p>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">The consequences of deprived sleep are well documented.  It is a fact that poor sleep leads to long term issues, especially in young children.  Sleep has tremendous effects on health, memory, emotions and academic potential.  No child should ever be deprived of a peaceful sleep.</p>
@@ -35,7 +38,7 @@ export default (props) => {
 						</div>
 					</Col>
 					<Col sm="6" className="mb-3 mb-sm-5 mt-3 mt-sm-5 px-0 px-sm-2 px-md-2 px-lg-4 px-xl-4">
-						<h5 className="erbaum-bold color-primary pb-2 pb-sm-5 text-uppercase">Core Values</h5>
+						<h5 className="erbaum-bold color-primary pb-2 pb-sm-5 text-uppercase"><ScrollAnimation animateIn="fadeInUp">Core Values</ScrollAnimation></h5>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">Sleeping Children Around The World and their core values:</p>
 						<ul className="filson-pro-reg color-secondary text-1" style={{listStyle:'decimal',lineHeight:'24px'}}>
 							<li>It is the basic right of every child to have to have a comfortable night’s sleep.</li>
@@ -49,7 +52,7 @@ export default (props) => {
 						</div>
 					</Col>
 					<Col sm="6" className="mb-3 mb-sm-5 mt-3 mt-sm-5 px-0 px-sm-2 px-md-2 px-lg-4 px-xl-4">
-						<h5 className="erbaum-bold color-primary pb-2 pb-sm-5 text-uppercase">What is a Bedkit?</h5>
+						<h5 className="erbaum-bold color-primary pb-2 pb-sm-5 text-uppercase"><ScrollAnimation animateIn="fadeInUp">What is a Bedkit?</ScrollAnimation></h5>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">Bedkits have benefited more than a million children in 34 developing countries around the world and are largely comprised of the following items:</p>
 						<ul className="filson-pro-reg color-secondary text-1" style={{lineHeight:'24px'}}>
 							<li>A mat or mattress/bedding</li>
@@ -63,7 +66,7 @@ export default (props) => {
 						</div>
 					</Col>
 					<Col sm="6" className="mb-3 mb-sm-5 mt-3 mt-sm-5 px-0 px-sm-2 px-md-2 px-lg-4 px-xl-4">
-						<h5 className="erbaum-bold color-primary pb-2 pb-sm-5 text-uppercase">Volunteers</h5>
+						<h5 className="erbaum-bold color-primary pb-2 pb-sm-5 text-uppercase"><ScrollAnimation animateIn="fadeInUp">Volunteers</ScrollAnimation></h5>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">Volunteerism takes on a new definition at Sleeping Children, because it means more than just giving back. They believe their volunteers get a lot more than they give; they actually nourish their own humanity.</p>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">At Sleeping Children there are three distinct kinds of volunteers required to mount a global initiative such as ours.  More than 2000 volunteers are from all over the world.</p>
 						<div className="charity-image text-center w-100">
@@ -71,7 +74,7 @@ export default (props) => {
 						</div>
 					</Col>
 					<Col sm="6" className="mb-3 mb-sm-5 mt-3 mt-sm-5 px-0 px-sm-2 px-md-2 px-lg-4 px-xl-4">
-						<h5 className="erbaum-bold color-primary pb-2 pb-sm-5 text-uppercase">1 Bedkit for Every Chirofoam Mattress</h5>
+						<h5 className="erbaum-bold color-primary pb-2 pb-sm-5 text-uppercase"><ScrollAnimation animateIn="fadeInUp">1 Bedkit for Every Chirofoam Mattress</ScrollAnimation></h5>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">With every purchase of a Chirofoam mattress, we will donate a Bedkit to a child in need. A bedkit provides a child with the means to go on thriving and the promise that the world holds a brighter future.</p>
 						<p className="filson-pro-reg color-secondary pt-2 text-1">These Bedkits provide a child with the basic necessities for sleep. This transforms the life of that child and these tools have a lasting effect on the entire family.</p>
 						<div className="charity-image text-center w-100">
@@ -84,7 +87,7 @@ export default (props) => {
 		<section className="pb-0 pb-sm-0 pb-md-2 pb-lg-4 pb-xl-4">
 			<Container className="pb-0 pb-sm-5">
 				<Row className="pb-0 pb-sm-5 mx-0">
-					<h4 className="text-center color-primary erbaum-bold text-uppercase w-100 pt-2 pt-sm-5 mt-0 mt-sm-0 mt-md-0 mt-lg-5 mt-xl-5">Thank you for helping to provide a child in need with better sleep. </h4>
+					<h4 className="text-center color-primary erbaum-bold text-uppercase w-100 pt-2 pt-sm-5 mt-0 mt-sm-0 mt-md-0 mt-lg-5 mt-xl-5"><ScrollAnimation animateIn="fadeInUp">Thank you for helping to provide a child in need with better sleep. </ScrollAnimation></h4>
 					<p className="pt-0 text-uppercase text-center w-100 pt-2 pt-md-5" style={{color:'#ba2154'}}>For more information on Sleeping Children Around The World visit their website at <a href="https://www.scaw.org/#scawhome" style={{color:'#000'}}>www.scaw.org</a>.</p>
 				</Row>
 			</Container>
@@ -109,6 +112,7 @@ export default (props) => {
 				</Row>
 			</Container>
 		</section>
+<ScrollAnimation animateIn="fadeInUp">
 		<section className="rating-and-review py-3 py-sm-5 mb-4 mb-sm-0">
 			<Container className="pb-0 pb-sm-5">
 				<Row className="mx-0">
@@ -127,6 +131,7 @@ export default (props) => {
 				</Row>
 			</Container>
 		</section>
+</ScrollAnimation>
 		<Footer />
 	</>
   );
